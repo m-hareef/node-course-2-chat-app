332a5254c0c56dbe7f936cc02fa45d6f0e07db8d
Hareef <hareef@Hareefs-MBP.Home>
2017-09-25 20:37:20 +0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 //load built in module helping in setting paths especially when going one folder up
 const path = require('path');  //built in module, no need to install
+const http = require('http'); //built in module, used by express
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');  //as public path is one folder up and then the public folder
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //set root route to the public path
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+
+//Listen to a new connection
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  //Disconects
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
